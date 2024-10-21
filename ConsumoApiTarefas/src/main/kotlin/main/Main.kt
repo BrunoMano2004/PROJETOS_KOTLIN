186c7e7f6b14305af50b791336027e95ff0155ea
Bruno Mano <143518260+BrunoMano2004@users.noreply.github.com>
2024-10-21 16:05:34 -0300

    Melhorando o projeto e consumindo o endpoint de login

--- ConsumoApiTarefas/src/main/kotlin/main/Main.kt
+++ ConsumoApiTarefas/src/main/kotlin/main/Main.kt
@@ -1,5 +1,11 @@
 package main
 
+import model.LoginDto
+import service.ConsumoApiService
+
 fun main(){
 
+    val login = LoginDto("brunomano2004@gmail.com", "Bruno@8401")
+
+    println(ConsumoApiService.autenticar(login))
 }
--- ConsumoApiTarefas/src/main/kotlin/service/ConsumoApiService.kt
+++ ConsumoApiTarefas/src/main/kotlin/service/ConsumoApiService.kt
@@ -3,16 +3,23 @@ package service
 import model.LoginDto
 import java.net.URI
 import com.google.gson.Gson
+import model.TokenDto
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpRequest.BodyPublishers
 import java.net.http.HttpResponse
 
-class ConsumoApiService {
+object ConsumoApiService {
 
-    fun enviarRequisicaoGet(request: HttpRequest.Builder): String{
+    fun enviarRequisicao(json: String): String{
 
         val client = HttpClient.newHttpClient()
+
+        val request = HttpRequest.newBuilder()
+            .uri(URI.create("http://localhost:8080/login"))
+            .POST(BodyPublishers.ofString(json))
+            .header("Content-type", "application/json")
+            .build()
 
         val response = client.send(request, HttpResponse.BodyHandlers.ofString())
 
@@ -21,9 +28,16 @@ class ConsumoApiService {
 
     fun autenticar(login: LoginDto): String{
         val gson = Gson()
+        val json = gson.toJson(login)
 
-        val request = HttpRequest.newBuilder()
-            .uri(URI.create("http://localhost:8080/login"))
-            .POST(BodyPublishers.ofString(json))
+        val response = enviarRequisicao(json)
+        val tokenDto = gson.fromJson(response, TokenDto::class.java)
+
+        try {
+            val token = tokenDto.jwtToken
+            return token
+        } catch (ex: NullPointerException){
+            return ex.message.toString()
+        }
     }
 }
